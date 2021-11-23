25e50f8fe0a75fa93499d0463220ac7f431715cc
Diego Granados <diegogranados143@gmail.com>
2021-11-23 17:27:36 -0400

    Estrutura Basica html para index.php

--- index.php
+++ index.php
@@ -4,9 +4,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Unident</title>
 </head>
-<body>
-    
+<body <?php body_class(); ?>>
+    test
+    <header>
+        <section class='top-bar'>
+            <div class="phone"></div>
+            <div class="social-media-items"></div>
+        </section>
+        <section class="menu-area">
+            <div class="log"></div>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
+    <main class="content-area">
+        <section class="main-area">
+            <h1></h1>
+            <button></button>
+        </section>
+        <section class="especialidades">
+            <h1></h1>
+            <div class="slider"></div>
+        </section>
+        <section class='video'>
+            <div>
+                <img src="" alt="">
+                <h3></h3>
+            </div>
+            <div>
+                <video src=""></video>
+            </div>
+        </section>
+        <section class="team">
+            <h1></h1>
+            <div class="slider"></div>
+        </section>
+        <section class='video'>
+            <div>
+                mapa
+            </div>
+            <div>
+                <h1></h1>
+                <h3></h3>
+                <h2></h2>
+                <h2></h2>
+                <h2></h2>
+                <form action=""></form>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div>
+            <div>
+                <div>Logo</div>
+                <div>Social Media</div>                
+            </div>
+            <div>
+                <h2></h2>
+                <p></p>
+            </div>
+            <div>
+                <h2>Contacto</h2>                
+            </div>
+        </div>
+        <div>Politica de Privacidad</div>
+    </footer>
 </body>
 </html>
