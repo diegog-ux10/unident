e09f6b5c9d180a5a3c2620b72e2178fbe1057856
Diego Granados <diego@codeando.edu.pe>
2021-12-01 17:28:16 -0400

    Nuevo titulo

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Unident Prueba</title>
+    <title>Unident Prueba 2</title>
     <?php wp_head() ?>
 </head>
 <body <?php body_class(); ?>>
